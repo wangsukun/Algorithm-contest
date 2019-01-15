c36914a7791a861fb1087fb699c5a66d0dd68783
王家伟 <572264287@qq.com>
2019-01-15 21:59:55 +0800

    Create Triple_Mean_Value.c

---
+++ Triple_Mean_Value.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(void) { 
+    
+    int a,b,c;
+    double d ;
+    
+    scanf("%d%d%d", &a, &b, &c);
+    
+    d = (a + b + c)/3.0;
+    
+    printf("%.3f\n",d);
+    
+    
+    
+	return 0;
+}
