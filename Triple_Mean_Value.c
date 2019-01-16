f4d5434c5d9729474b77e768e85ddbd8795a7feb
王家伟 <572264287@qq.com>
2019-01-16 08:23:38 +0800

    Update Triple_Mean_Value.c

--- Triple_Mean_Value.c
+++ Triple_Mean_Value.c
@@ -4,13 +4,11 @@
 int main(void) { 
     
     int a,b,c;
-    double d ;
     
     scanf("%d%d%d", &a, &b, &c);
     
-    d = (a + b + c)/3.0;
     
-    printf("%.3f\n",d);
+    printf("%.3f\n", (a + b + c)/3.0);
     
     
     
