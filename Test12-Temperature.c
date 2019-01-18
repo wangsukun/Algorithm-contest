1e4e8ab28aec34ad2f7f8e699858ddd91ddf57ef
王家伟 <572264287@qq.com>
2019-01-18 21:12:41 +0800

    Create Test12-Temperature.c

---
+++ Test12-Temperature.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(void) { 
+    
+    float c,f;
+    
+    scanf("%f", &f);
+    
+    c = 5.0*(f - 32.0)/9.0;
+    
+    
+    printf("%.3f\n", c);
+    
+    
+    
+	return 0;
+}
