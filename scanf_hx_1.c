e277719405ed752e88c44bbc347849c6b626163c
王家伟 <572264287@qq.com>
2019-04-04 21:38:43 +0800

    Create scanf_hx_1.c

---
+++ scanf_hx_1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+  
+
+int main(void)
+{
+	unsigned short a = 0x5BA2;
+	unsigned short b = 0x12;
+	unsigned short c = 0x34;
+	unsigned char  d = 0x94;	
+	 
+		
+	printf("Input a:");
+	
+	scanf("%x",&a);
+	
+    printf("Result:\n");
+    printf("a = %X\n",a);
+    printf("b = %X\n",b);    
+	printf("c = %X\n",c);
+	printf("d = %X\n",d);
+	
+
+}
+
+
+
+
+
